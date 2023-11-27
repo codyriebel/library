1e8169ef9b4950fc94c7db4bda588579b78e9b48
Cody Riebel <cody.riebel@gmail.com>
2023-11-27 12:51:54 -0500

    feat: ability to add new books

--- script.js
+++ script.js
@@ -5,32 +5,48 @@ function Book(title, author, numPages, isRead) {
   this.title = title;
   this.author = author;
   this.numPages = numPages;
-  this.isRead = isRead ? 'read' : 'not read yet';
+  this.isRead = isRead ? "read" : "not read yet";
 }
 
-Book.prototype.info = function() {
-  return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.isRead}`
-};
+// Book.prototype.info = function() {
+//   return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.isRead}`
+// };
 
-const test = new Book('title', 'author', 42, true);
+const test = new Book("title", "author", 42, true);
 
-const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 
-function addBookToLibrary() {
-  // get book details from input fields
-  const newBook = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-  myLibrary.push(test);
-  myLibrary.push(newBook);
-}
-
-function displayBooks() {
-  const bookContainer = document.querySelector(".bookContainer");
-  const card = document.createElement(div.card);
-  bookContainer.appendChild(card);
-  for (const book of myLibrary) {
-    bookDiv.textContent += book.info();
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  const bookTable = document.querySelector(".bookTable tbody");    const row = document.createElement("tr");
+  bookTable.appendChild(row);
+  for (const bookData in book) {
+    const td = document.createElement("td");
+    td.textContent = book[bookData];
+    row.appendChild(td);
   }
 }
 
-addBookToLibrary();
-displayBooks();
+const dialog = document.querySelector("dialog");
+const newBookButton = document.querySelector(".newBook");
+const submitNewBook = document.querySelector(".submit");
+const cancel = document.querySelector(".cancel");
+
+
+newBookButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+submitNewBook.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const titleInput = document.querySelector("dialog .title");
+  const authorInput = document.querySelector("dialog .author");
+  const pagesInput = document.querySelector("dialog .pages");
+  const readBox = document.querySelector("dialog .read");
+
+  const newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, readBox.checked);
+  dialog.close();
+
+  addBookToLibrary(newBook);
+});
